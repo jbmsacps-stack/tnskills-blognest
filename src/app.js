7e785a8612912800d65f076e7db37456a4db3aa6
jbmsacps-stack <jbmsacps@gmail.com>
2026-09-24 17:51:51 +0530

    Created authController.js authRoutes.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
@@ -8,6 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use("/api/auth", authRoutes);
 
 // Health check
 app.get("/", (req, res) => {
